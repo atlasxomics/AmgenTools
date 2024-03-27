31984b7a71a76ba49b444f1bc4bc067ea6f5a3cb
JoshuaBAtlas <84153010+JoshuaBAtlas@users.noreply.github.com>
2024-03-27 14:38:29 -0400

    Update storage.py

--- src/storage.py
+++ src/storage.py
@@ -401,7 +401,7 @@ class StorageAPI:
             return utils.error_message("The file doesn't exists",status_code=404)
         else:
             try:
-                command = f"echo 'aws s3 presign s3://{bucket_name}/{filename}' > /hostpipe/the_pipe"
+                command = f"echo 'aws s3 presign s3://{bucket_name}/{filename}' > pipe/the_pipe"
                 subprocess.run(command, shell=True)
                 text = open('/hostpipe/output.txt')
                 url = text.readline()
